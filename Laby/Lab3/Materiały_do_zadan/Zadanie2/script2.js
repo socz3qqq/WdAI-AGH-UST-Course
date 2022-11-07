91927457a97103c03606d90a5f1824b16317f351
socz3qqq <soczawa@student.agh.edu.pl>
2022-11-08 00:52:33 +0100

    LAB3 start

---
+++ Laby/Lab3/Materiały_do_zadan/Zadanie2/script2.js
@@ -0,0 +1,8 @@
+const images = ['','','']
+const photo = document.getElementsByTagName(img)
+.setAttribute('src', images[0])
+
+
+
+.setAttribute("src", images[flag%ile + 1])
+.className = "image"+ flag%ile
